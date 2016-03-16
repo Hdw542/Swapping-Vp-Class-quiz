dd4c85adec8f208253ac63a6d7311d9f317b0276
Hdw542 <hdw542@hotmail.com>
2016-03-16 19:05:17 +0500

    swap two values without using third variable

---
+++ swapping/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swapping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // class + quiz
+            // swap two numbers without using third variable
+            int var1 = 10;
+            int var2 = 20;
+            var1 = var1 + var2; //var1 = 30
+            var2 = var1 - var2; //var2 swapped & equal 10
+            var1 = var1 - var2; //var1 swapped & equal 20
+
+            Console.Out.WriteLine("Variable 1 = " + var1);
+            Console.Out.WriteLine("Variable 2 = " + var2);
+            Console.ReadLine();
+        }
+    }
+}
